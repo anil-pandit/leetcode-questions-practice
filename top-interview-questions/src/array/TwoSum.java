c692306d2e4250b5278c85bf8b861c9f5a2cf73d
Anil Kumar Pandit <anilkumarpandit@Anils-MacBook-Air.local>
2023-01-29 01:09:44 +0530

    first commit

---
+++ top-interview-questions/src/array/TwoSum.java
@@ -0,0 +1,28 @@
+package array;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+
+            if(map.containsKey(target-nums[i])){
+                return new int[]{i, map.get(target-nums[i])};
+            }
+            else{
+                map.put(nums[i], i);
+            }
+        }
+        return new int[]{-1, -1};
+    }
+    public static void main(String[] args) {
+        int nums[] = new int[]{2, 7, 11, 15};
+        int res[] = twoSum(nums, 9);
+        System.out.println(Arrays.toString(res));
+    }
+}
